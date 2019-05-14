114cc0d0aec917b85f70733e9e9ef84aac1d20fc
Mwrench <mateus.alves.quintela@gmail.com>
2019-05-14 16:39:58 +0000

    template novo

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Mateus Quintela'
 
+@app.route('/index.html')
+def index():
+    return 'index.html'
+
 if __name__== '__main__':
     app.run()
